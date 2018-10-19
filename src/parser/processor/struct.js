8df57d36f14081f09787b4ce530edcd54405451a
lushijie <lushijie03@meituan.com>
2018-10-19 18:25:50 +0800

    remove defaultValue

--- src/parser/processor/struct.js
+++ src/parser/processor/struct.js
@@ -14,10 +14,10 @@ module.exports = function(ast, thriftTool) {
       required: ele.required,
     }
 
-    // 默认值，目前仅支持常量，有待加强
-    if (ele.defaultValue) {
-      field.defaultValue = thriftTool.resolveMixValue(ele.defaultValue);
-    }
+    // // 默认值，目前仅支持常量，有待加强; 暂不支持 defaultValue
+    // if (ele.defaultValue) {
+    //   field.defaultValue = thriftTool.resolveMixValue(ele.defaultValue);
+    // }
 
     field = Utils.extend(field, thriftTool.resolveMixType(ele.valueType));
     res[identifier][key] = field;
