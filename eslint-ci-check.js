9454267115c223ebf8f2ceb808e91ae71fc460f8
lushijie <lushijie03@meituan.com>
2018-11-01 14:43:30 +0800

    eslint

---
+++ eslint-ci-check.js
@@ -0,0 +1,45 @@
+/* eslint-disable */
+const exec = require('child_process').exec;
+const CLIEngine = require('eslint').CLIEngine;
+const cli = new CLIEngine({});
+
+function getErrorLevel(number) {
+  switch (number) {
+    case 2:
+      return 'error';
+    case 1:
+      return 'warn';
+    default:
+  }
+  return 'undefined';
+}
+let pass = 0;
+exec('git diff --cached --name-only| grep .js$', (error, stdout) => {
+  if (stdout.length) {
+    const array = stdout.split('\n');
+    array.pop();
+    const results = cli.executeOnFiles(array).results;
+    let errorCount = 0;
+    let warningCount = 0;
+    results.forEach((result) => {
+      errorCount += result.errorCount;
+      warningCount += result.warningCount;
+      if (result.messages.length > 0) {
+        console.log('\n');
+        console.log(result.filePath);
+        result.messages.forEach((obj) => {
+          const level = getErrorLevel(obj.severity);
+          console.log(`   ${obj.line}:${obj.column}  ${level}  ${obj.message}  ${obj.ruleId}`);
+          pass = 1;
+        });
+      }
+    });
+    if (warningCount > 0 || errorCount > 0) {
+      console.log(`\n   ${errorCount + warningCount} problems (${errorCount} ${'errors'} ${warningCount} warnings)`);
+    }
+    process.exit(pass);
+  }
+  if (error !== null) {
+    console.log(`exec error: ${error}`);
+  }
+});
--- src/index.js
+++ src/index.js
@@ -4,5 +4,4 @@
  * @Last Modified by:   lushijie
  * @Last Modified time: 2018-09-30 18:22:07
  */
-console.log(123);
 module.exports = require('./thrift-tool');
